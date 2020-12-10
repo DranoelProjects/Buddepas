847a2b4929ee9054f8083542a9d3843422926534
Wambergue Léonard <leonard.wamberguepro@gmail.com>
2020-12-10 23:00:48 +0100

    popup_add_recipe_to_calendar

--- app/src/main/java/com/cours/buddepas/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/cours/buddepas/ui/calendar/CalendarFragment.java
@@ -1,11 +1,24 @@
 package com.cours.buddepas.ui.calendar;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,12 +37,19 @@ import com.cours.buddepas.tools.ApiManager;
 import com.cours.buddepas.tools.Singleton;
 import com.cours.buddepas.ui.calendar.fragments.DayFragment;
 import com.cours.buddepas.ui.calendar.fragments.ProgramFragment;
+import com.cours.buddepas.ui.recipe.AddNewRecipeActivity;
 import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
 import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import org.joda.time.DateTime;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class CalendarFragment extends Fragment {
     //Instances
@@ -51,6 +71,63 @@ public class CalendarFragment extends Fragment {
 
         viewPager = fragment.findViewById(R.id.calendar_view_pager);
         tabLayout = fragment.findViewById(R.id.calendar_tab_layout);
+
+        //program recipe btn
+        ImageButton programRecipeButton = fragment.findViewById(R.id.program_recipe_button);
+        programRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // inflate the layout of the popup window
+                LayoutInflater inflater = (LayoutInflater)
+                        getActivity().getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_program_recipe, null);
+
+                //initalizing views in popup
+                Button btnCancel = popupView.findViewById(R.id.close_calendar_pop_up);
+                Button btnSubmit = popupView.findViewById(R.id.add_recipe_to_calendar_button);
+                EditText inputPeopleNumber = popupView.findViewById(R.id.calendar_input_people_number);
+
+                //DatePicker
+                final Calendar myCalendar = Calendar.getInstance();
+                final EditText editTextDatePicker= (EditText) popupView.findViewById(R.id.calendar_popup_date_picker);
+                final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                          int dayOfMonth) {
+                        // TODO Auto-generated method stub
+                        myCalendar.set(Calendar.YEAR, year);
+                        myCalendar.set(Calendar.MONTH, monthOfYear);
+                        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        updateLabel(editTextDatePicker, myCalendar);
+                    }
+                };
+                editTextDatePicker.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // TODO Auto-generated method stub
+                        new DatePickerDialog(getActivity(), date, myCalendar
+                                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                                myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                    }
+                });
+
+                // create the popup window
+                int width = LinearLayout.LayoutParams.MATCH_PARENT;
+                int height = LinearLayout.LayoutParams.MATCH_PARENT;
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+                popupWindow.setOutsideTouchable(false);
+                // show the popup window
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                //Close the pop up
+                btnCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        popupWindow.dismiss();
+                    }
+                });
+            }
+        });
         return fragment;
     }
 
@@ -85,5 +162,11 @@ public class CalendarFragment extends Fragment {
         adapter.addFragment(new ProgramFragment(), "Programme");
 
         viewPager.setAdapter(adapter);
+    }
+
+    private void updateLabel(EditText editTextDatePicker, Calendar myCalendar) {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        editTextDatePicker.setText(sdf.format(myCalendar.getTime()));
     }
 }
